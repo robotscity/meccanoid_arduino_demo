d0d4cb3c72c1e7ce3678a2c547cc13c8739ce57b
Tatyana Volkova <tatyana.a.volkova@gmail.com>
2016-01-20 15:43:15 +0300

    Update demo_meccanoids.ino

--- demo_meccanoids.ino
+++ demo_meccanoids.ino
@@ -4,7 +4,7 @@
 //0) Meccanoid G15 in its human form
 //1) Any Arduino
 //2) Motor shield or motor driver, to drive robot's wheels. I tried the one based on L293D and it was quite decent
-//3) Tamiya Mini male connector with wires - to connect to the robots' battery
+//3) Tamiya Mini female connector with wires - to connect to the robots' battery
 //4) Male connectors: CWF-2 to connect one motor and PLS-2 to connect another
 //5) 3 resistors of 22 kOhm and 3 resistors of 1 kOhm - to make pullup resistors (according to the manual)
 //6) Download the library: http://cdn.meccano.com/open-source/meccanoid-library.zip
